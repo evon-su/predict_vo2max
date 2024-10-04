8c519318557cc1ec9034d754924cf34992b84592
elaine <sessene220@gmail.com>
2024-10-04 11:29:07 +0800

    update

--- submit/pipeline.py
+++ submit/pipeline.py
@@ -26,12 +26,15 @@ def pipeline(model,
 
     # GET DATA
     info, actdf = get_info(win=speed_win, hrr_range=hrr_range, speed_range=speed_range, visual=plot)
+    # for col in ['ratio_med', 'gender', 'hr_rest']:
+    #     actdf[f'{col}_standard'] = actdf[col].apply(lambda x: (x - actdf[col].mean()) / actdf[col].std())
 
     # SPLIT TRAIN & TEST BY USER
     if split_method == 'workout':
         train_ls, test_ls = preprocess.get_train_test_ls(len(actdf), train_ratio, rand=random_state)
         train = actdf.iloc[train_ls]
         test = actdf.iloc[test_ls]
+
     elif split_method == 'user':
         train_user_ls, test_user_ls = preprocess.get_train_test_ls(len(info), train_ratio, rand=random_state)
         user_ls = [user for user in info]
@@ -71,7 +74,7 @@ def pipeline(model,
     y_train_men = train['real_vo2max'].values
 
     rf = LinearRegression()
-    # rf = RandomForestRegressor()
+    # rf = RandomForestRegressor(max_depth=5, max_leaf_nodes=5)
     rf.fit(X_train, y_train_men)
     # joblib.dump(rf, 'ml_model.pkl')
     print("MACHINE LEARNING VARIABLES: ", vars(rf))
@@ -224,99 +227,6 @@ def fit_polyfit2(ratio_ls, vo2max_ls, degree=1):
     coef = np.polyfit(ratio_ls, vo2max_ls, degree)
 
     return coef
-
-
-def pipeline(model,
-             degree=1,
-             speed_win=3,
-             hrr_range=(0.7, 0.92),
-             speed_range=(3, 30),
-             train_ratio=0.8,
-             random_state=0,
-             plot=True):
-    ml_cols = ['ratio_med', 'gender', 'hr_rest']
-
-    # GET DATA
-    info, actdf = get_info(win=speed_win, hrr_range=hrr_range, speed_range=speed_range, visual=plot)
-
-    # SPLIT TRAIN & TEST BY USER
-    user_ls = [user for user in info]
-    train_user_ls, test_user_ls = preprocess.get_train_test_ls(len(info), train_ratio, rand=random_state)
-    train_user_ls = [user_ls[i] for i in train_user_ls]
-    test_user_ls = [user_ls[i] for i in test_user_ls]
-    train = actdf.loc[actdf.index.isin(train_user_ls)]
-    test = actdf.loc[actdf.index.isin(test_user_ls)]
-
-    coef = fit_polyfit2(train.ratio_med.to_list(), train.real_vo2max.to_list(), degree=degree)
-    print("REGRESSION COEF : ", coef)
-
-    # Visualization
-    x = np.linspace(5, 22, 100)
-    y_train = 0
-    for i in range(len(coef)):
-        y_train += coef[i] * x**(len(coef)-i-1)
-    if plot:
-        corr = np.corrcoef(train.ratio_med.to_list(), train.real_vo2max.to_list())[0][1]
-        print("CORR = ", corr)
-        plt.scatter(train.ratio_med, train.real_vo2max, alpha=0.4, color=train.c)
-        plt.plot(x, y_train, linestyle='--', alpha=0.4, c='gray')
-        plt.title(f"(Training) corr={corr:.3f}")
-        plt.xlabel('ratio of speed / hrr')
-        plt.ylabel('vo2max')
-        plt.show()
-
-    # MACHINE LEARNING
-    X_train = train[ml_cols].values
-    y_train = train['real_vo2max'].values
-
-    rf = LinearRegression()
-    rf.fit(X_train, y_train)
-    # joblib.dump(rf, 'ml_model.pkl')
-    y_pred = rf.predict(X_train)
-    rmse = evaluate.get_mse(y_train, y_pred) ** 0.5
-    r2 = evaluate.get_rsq(y_train, y_pred)
-    print(f"Training Result: rmse={rmse: .1f}, r2={r2:.2f}", )
-
-    # PREDICT
-    output = []
-    for workout_number in test.workout_number:
-        user = test[test.workout_number==workout_number].index[0]
-        print("user, workout : ", user, workout_number, end=', ')
-        workout = get_workout_df(user, workout_number)
-
-        # PREDICTING
-        vo2max_predict = predictVO2max(info[user], workout.SPEED, workout.HEART_RATE, model())
-
-        vo2max_real = test[test['workout_number'] == workout_number]['real_vo2max'].iloc[0]
-        output.append([user, workout_number, -1, vo2max_predict, vo2max_real])
-        print("vo2max : ", round(vo2max_predict, 1), f"({vo2max_real})")
-
-    # EVALUATE
-    output = pd.DataFrame(output, columns=['user', 'workout_number', 'ratio_med', 'vo2max_pred', 'vo2max_real'])
-    rmse_1 = evaluate.get_mse(output.vo2max_real, output.vo2max_pred) ** 0.5
-    r2_1 = evaluate.get_rsq(output.vo2max_real, output.vo2max_pred)
-    print(f"RMSE={rmse_1:.1f}, R2={r2_1:.3f}")
-
-    output['diff'] = abs(output.vo2max_real - output.vo2max_pred)
-    output['filename'] = output.apply(lambda row: f"{row.user}_{row.workout_number}", axis=1)
-    output.to_csv("out.csv")
-
-    print(f"ML: RMSE={rmse_1:.1f}, R2={r2_1:.3f}")
-
-    if plot:
-        corr = np.corrcoef(output.ratio_med, output.vo2max_pred)[0][1]
-        x = np.linspace(5, 22, 100)
-        y_train = 0
-        for i in range(len(coef)):
-            y_train += coef[i] * x ** (len(coef) - i - 1)
-        plt.scatter(output.vo2max_real, output.vo2max_pred, alpha=0.4)
-        plt.plot(output.vo2max_real, output.vo2max_real, alpha=0.3, c='gray', linestyle='--')
-        plt.title(f"(Testing by Func) rmse={rmse_1:.2f}, r2={r2_1:.2f}")
-        plt.xlabel('vo2max_real')
-        plt.ylabel('vo2max_predict')
-        plt.legend()
-        plt.show()
-    return rmse_1, r2_1
 
 
 if __name__ == '__main__':
